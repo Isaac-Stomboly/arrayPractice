24b2cc91579a47ee2a52829bc4d86fcee764c45a
STOMBOLY <is0903892@otc.edu>
2018-10-30 08:53:51 -0500

    changed file

--- firstProject.py
+++ firstProject.py
@@ -1,9 +1,14 @@
+import random
 aryList = ["biscuits", "sweets", "tea"]
 
 for i in range(3):
     print("What else would you like to add?")
     choice = input()
     aryList.append(choice)
-print("###################################################################################################################")
+print("#########################################")
 for i in range(6):
     print(aryList[i])
+
+
+number = random.randint(0, 5)
+print(aryList[number])
