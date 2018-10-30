31f2407bad193f22683c74498b83df80a3955f96
STOMBOLY <is0903892@otc.edu>
2018-10-30 08:49:21 -0500

    changed file

--- firstProject.py
+++ firstProject.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+aryList = ["biscuits", "sweets", "tea"]
+
+for i in range(3):
+    print("What else would you like to add?")
+    choice = input()
+    aryList.append(choice)
+print("###################################################################################################################")
+for i in range(6):
+    print(aryList[i])
